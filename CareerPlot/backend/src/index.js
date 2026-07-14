564ff5caf10ba408c9fb9fff580615285c244984
N.Varshitha <varshithanallamada@gmail.com>
2026-07-14 10:26:35 +0530

    Fix Express v5 wildcard route syntax error

--- CareerPlot/backend/src/index.js
+++ CareerPlot/backend/src/index.js
@@ -29,7 +29,7 @@ app.use('/api', apiRoutes);
 const frontendPath = path.join(__dirname, '../../../frontend/dist');
 app.use(express.static(frontendPath));
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(frontendPath, 'index.html'));
 });
 
